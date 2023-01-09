b78d4f17970f2c0902fb62735bdc733585d1576f
BillyMiles <williammiles201@gmail.com>
2023-01-09 17:56:19 -0500

    migration added

---
+++ db/migrate/20230109225457_creat_birds_table.rb
@@ -0,0 +1,11 @@
+class CreatBirdsTable < ActiveRecord::Migration[7.0]
+  def change
+    create_table :birds do |t|
+      t.string :name
+      t.string :species
+      t.string :name
+      t.text :description
+
+      t.timestamps
+    end
+end
